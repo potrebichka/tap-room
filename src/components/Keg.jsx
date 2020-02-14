d6ff5a1908a4434bf49eecf75a3995646f0b5e66
Nina Potrebich <potrebich@gmail.com>
2020-02-14 14:24:55 -0800

    Change Keg Component to statefull Component. Add quantity of pints to state. Connect sold button to state.

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -4,32 +4,48 @@ import { Link } from 'react-router-dom';
 import EditKeg from './EditKeg';
 import {Button} from 'react-bootstrap';
 
-const Keg = (props) => {
-  return (
-    <div className='keg'>
-      <style jsx>{`
-        .keg {
-            background: rgba(0,0,0,0.5);
-            min-width: 300px;
-            padding: 10px;
-            margin: 10px;
-            text-align: center;
-        }
-      `}</style>
-      <h3>{props.name}</h3>
-      <p><em>{props.brand}</em></p>
-      <p>${props.price}</p>
-      <p>{props.alcoholContent}% ABV</p>
-      <p>{props.ibu ? `${props.ibu} IBU`: 'NO IBU'}</p>
-      {props.employee ? 
-      <div>
-        <p>Pints left: {props.pints <= 0 ? 0 : props.pints}</p>
-        <Button className="btn btn-success">Sold one pint</Button><br/>
-        <Link to="/edit" component ={EditKeg}>Edit</Link>
-      </div> 
-      : null}
-    </div>
-  );
+class Keg extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pints: 124
+    };
+  }
+  
+  render() {
+    return (
+      <div className='keg'>
+        <style jsx>{`
+          .keg {
+              background: rgba(0,0,0,0.5);
+              min-width: 300px;
+              padding: 10px;
+              margin: 10px;
+              text-align: center;
+          }
+        `}</style>
+
+        <h3>{this.props.name}</h3>
+        <p><em>{this.props.brand}</em></p>
+        <p>${this.props.price}</p>
+        <p>{this.props.alcoholContent}% ABV</p>
+        <p>{this.props.ibu ? `${this.props.ibu} IBU`: 'NO IBU'}</p>
+
+        {this.props.employee ? 
+        <div>
+          <p>Pints left: {this.state.pints <= 0 ? 0 : this.state.pints}</p>
+
+          <Button className="btn btn-success" onClick={() => this.setState({pints: this.state.pints-1})
+  }>Sold one pint</Button><br/>
+
+          <EditKeg />
+
+        </div> 
+        : null}
+
+      </div>
+    );
+  }
 };
 
 Keg.propTypes = {
@@ -38,7 +54,6 @@ Keg.propTypes = {
   price: PropTypes.number.isRequired,
   alcoholContent: PropTypes.number.isRequired,
   ibu: PropTypes.number,
-  pints: PropTypes.number.isRequired,
   employee: PropTypes.bool
 };
 
--- src/components/KegList.jsx
+++ src/components/KegList.jsx
@@ -27,15 +27,15 @@ const KegList = (props) => {
       <div className="text-box">
         <h1>List of available kegs:</h1>
         <div className="kegs-list">
-          <Keg name="Gold" brand="Saint Archer Brewing Company" price={7.00} alcoholContent={4.2} pints={124} employee={props.employee}/>
-          <Keg name="Insignia" brand="Mystic Brewery" price={8.50} alcoholContent={5.0} pints={124} employee={props.employee}/>
-          <Keg name="Easy Jack" brand="Firestone Walker Brewing Company" price={7.50} alcoholContent={4.0} ibu={47} pints={124} employee={props.employee}/>
+          <Keg name="Gold" brand="Saint Archer Brewing Company" price={7.00} alcoholContent={4.2} employee={props.employee}/>
+          <Keg name="Insignia" brand="Mystic Brewery" price={8.50} alcoholContent={5.0} employee={props.employee}/>
+          <Keg name="Easy Jack" brand="Firestone Walker Brewing Company" price={7.50} alcoholContent={4.0} ibu={47} employee={props.employee}/>
           <Keg name="Pale Ale" brand="Sierra Nevada Brewing Co." price={7.00} alcoholContent={5.6} ibu={38} pints={124}employee={props.employee}/>
-          <Keg name="Golden Monkey" brand="Victory Brewing Company" price={8.00} alcoholContent={9.5} ibu={25} pints={124} employee={props.employee}/>
-          <Keg name="Margarita Gose" brand="Cigar City Brewing" price={7.50} alcoholContent={4.2} ibu={5} pints={124} employee={props.employee}/> 
-          <Keg name="Fat Tire" brand="New Belgium Brewing Company" price={7.00} alcoholContent={5.2} ibu={22} pints={124} employee={props.employee}/>
-          <Keg name="Guinness Draught" brand="Guinness" price={8.00} alcoholContent={4.2} ibu={45} pints={124} employee={props.employee}/>
-          <Keg name="Ginger Root Kombucha" brand="Urban Farm Fermentory" price={9.00} alcoholContent={1.5} pints={124} employee={props.employee}/>
+          <Keg name="Golden Monkey" brand="Victory Brewing Company" price={8.00} alcoholContent={9.5} ibu={25} employee={props.employee}/>
+          <Keg name="Margarita Gose" brand="Cigar City Brewing" price={7.50} alcoholContent={4.2} ibu={5} employee={props.employee}/> 
+          <Keg name="Fat Tire" brand="New Belgium Brewing Company" price={7.00} alcoholContent={5.2} ibu={22} employee={props.employee}/>
+          <Keg name="Guinness Draught" brand="Guinness" price={8.00} alcoholContent={4.2} ibu={45} employee={props.employee}/>
+          <Keg name="Ginger Root Kombucha" brand="Urban Farm Fermentory" price={9.00} alcoholContent={1.5} employee={props.employee}/>
         </div>
       </div>
 
